d165b692cee4f735a103066d9d4d46aa8651cbbc
truongmankham81dev <38904185+truongmankham81dev@users.noreply.github.com>
2019-02-10 11:59:43 +0700

    Update SayHellot.php

--- src/SayHellot.php
+++ src/SayHellot.php
@@ -4,7 +4,7 @@ class SayHello
 {
     public static function world()
     {
-        return 'Hello World, Composer Branch Test!!!!<br />';
+        return 'Hello World, Composer Test branch!!!!<br />';
     }
 }
 ?>
